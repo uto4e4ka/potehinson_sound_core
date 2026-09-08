ba7d0ec71faeee1ce2bc5607362a94589d9d11cc
azhestkov <alezhestkov@gmail.com>
2026-09-08 19:52:00 +0400

    register auto

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from typing import cast, Awaitable
 from potehinsonnet.net_models.discord_models import Command
 
 from containers.SoundCoreContainer import SoundCoreContainer
-from potehinsonnet.steup.command_registrator import CommandRegistrator
+from importlib.metadata import version
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -13,14 +13,26 @@ async def main():
 
     sound_core_container = SoundCoreContainer()
     sound_core_container.config.plugin.name.from_value("potehinson_sound_core")
-    sound_core_container.config.plugin.label.from_value("potehinson_discord_core")
+    sound_core_container.config.plugin.label.from_value("[🔊] Potehinson Sound Core ")
+    sound_core_container.config.plugin.type.from_value("plugin")
+    sound_core_container.config.plugin.description.from_value("Воспроизведение, управление звуками.\nПриветствие и ивенты.")
+    sound_core_container.config.plugin.icon.from_value("")
+    sound_core_container.config.plugin.author.from_value("Uto4ka404")
+    sound_core_container.config.plugin.version.from_value(
+        "0.0.1"
+    )
+    sound_core_container.config.plugin.site.from_value("https://potehinson-sound-core")
     sound_core_container.config.is_greeting.from_env("GREETING")
     sound_core_container.config.greeting_sound.from_env("GREETING_SOUND")
     await sound_core_container.init_resources()
 
+    try:
+        await asyncio.Event().wait()
+    finally:
+        print("Shutting down")
+        await sound_core_container.shutdown_resources()
 
 
 
 
-    await asyncio.Event().wait()
 asyncio.run(main())
